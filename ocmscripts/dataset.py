b1b987eeff94297fbeb0323740d111656fa750ce
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-03-20 16:41:59 +0100

    add and run basic data processing stage

--- ocmscripts/dataset.py
+++ ocmscripts/dataset.py
@@ -1,28 +1,52 @@
 from pathlib import Path
+import warnings
 
 from loguru import logger
+import lydata
+from lydata import C
+from lydata.utils import infer_and_combine_levels
 from tqdm import tqdm
 import typer
 
-from ocmscripts.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from ocmscripts.config import PROCESSED_DATA_DIR
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import pandas as pd  # noqa: E402
 
 app = typer.Typer()
 
 
+def compile_icd_codes(start: int = 2, end: int = 6) -> list[str]:
+    """Compile a list of ICD codes from the given range."""
+    icd_codes = []
+    for i in range(start, end + 1):
+        base = f"C{i:02d}"
+        icd_codes += [base] + [f"{base}.{j}" for j in range(10)]
+    return icd_codes
+
+
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
+    input_paths: list[Path],
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+    """Combine multiple datasets and filter for oral cavity subsites."""
+    datasets = []
+    for input_path in input_paths:
+        dataset = pd.read_csv(input_path, header=[0,1,2])
+        dataset = infer_and_combine_levels(dataset)
+        datasets.append(dataset)
+        logger.info(f"Loaded dataset from {input_path = }")
+
+    joined_dataset = pd.concat(datasets, axis="index", ignore_index=True)
+    logger.info(f"Concatenated {joined_dataset.shape = }")
+
+    is_oral_cavity = C("subsite").isin(compile_icd_codes(2, 6))
+    filtered_dataset = joined_dataset.ly.query(query=is_oral_cavity)
+    logger.info(f"Remaining {filtered_dataset.shape = }")
+
+    filtered_dataset.to_csv(output_path, index=False)
+    logger.success(f"Saved dataset to {output_path = }")
 
 
 if __name__ == "__main__":
